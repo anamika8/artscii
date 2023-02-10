384dc293addc4e0a2fa90b67dd0ea57ab8cc0841
Anamika Nayak <anayak@pdx.edu>
2023-02-09 23:02:40 -0800

    removed redundant console log

--- artscii/src/app/App.js
+++ artscii/src/app/App.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import Header from '../components/header/Header';
 import { getGiphyImageBySearchText } from '../services/giphyService';
 
+const DEFAULT_IMAGE_TO_SHOW = `https://media.giphy.com/media/fVeAI9dyD5ssIFyOyM/giphy.gif`;
+
 function App() {
 
     const [imageUrl, setImageUrl] = useState('')
@@ -12,6 +14,10 @@ function App() {
             .then(imageUrl => {
                 console.log(`Image URL received in UI - ${imageUrl}`)
                 setImageUrl(imageUrl)
+            })
+            .catch(err => {
+                console.log("error encountered = " + err);
+                setImageUrl(DEFAULT_IMAGE_TO_SHOW);
             });
     }, [])
 
@@ -23,6 +29,7 @@ function App() {
         <p>Site under construction.</p>
         </div>
     );
+
 }
 
 export default App;
--- artscii/src/services/giphyService.js
+++ artscii/src/services/giphyService.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const GIPHY_API_KEY = `7bcbIq6rDmXAuPqfDLaa1TrL7dN0q47w`;
 const DEFAULT_IMAGE_TO_SHOW = `https://media.giphy.com/media/fVeAI9dyD5ssIFyOyM/giphy.gif`;
 
+// public function that can be called by the UI or other services
 export async function getGiphyImageBySearchText(searchText = "",
     size = 'original',
     limit = 1,
@@ -13,26 +14,24 @@ export async function getGiphyImageBySearchText(searchText = "",
         searchText = defaultSearchText;
     }
     searchText = searchText.split(' ').join('+');
-    console.log(`q = ${searchText}`);
+    // creates the final API call
     const giphyApiCall = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${searchText}&limit=${limit}&offset=0&rating=${rating}&lang=${lang}`;
 
+    // getting the response from Giphy API
     const giphyResponse = await callGiphyApiWithSearchText(giphyApiCall);
     if (giphyResponse === undefined || giphyResponse.length === 0) {
         // array does not exist or is empty
         console.log(`No response from giphy - hence displaying default image url`);
         return DEFAULT_IMAGE_TO_SHOW;
     }
-    console.log(`Giphy API Response = ${giphyResponse}`)
-        // return the original size image
-    console.log(`Giphy Response url = ${giphyResponse.images.original.url}`)
-    return giphyResponse.images.original.url;
+    // return the original size image
+    return giphyResponse.images[`${size}`].url;
 }
 
+// function to call the Giphy API
 async function callGiphyApiWithSearchText(giphyApiCall) {
-    console.log(`Calling the API - ${giphyApiCall}`)
     try {
         const response = await axios.get(giphyApiCall);
-        console.log('response  - ', response.data.data[0])
         return response.data.data[0];
     } catch (error) {
         console.log(`Encountered error`)
