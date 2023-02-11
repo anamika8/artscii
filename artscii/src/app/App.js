ec1758232b8dfd0f1339034fa78438be351cb453
Anamika Nayak <anayak@pdx.edu>
2023-02-10 18:53:20 -0800

    removed api call from react

--- artscii/src/app/App.js
+++ artscii/src/app/App.js
@@ -1,34 +1,16 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import Header from '../components/header/Header';
-import { getGiphyImageBySearchText } from '../services/giphyService';
-
-const DEFAULT_IMAGE_TO_SHOW = `https://media.giphy.com/media/fVeAI9dyD5ssIFyOyM/giphy.gif`;
 
 function App() {
 
-    const [imageUrl, setImageUrl] = useState('')
-
-    useEffect(() => {
-        getGiphyImageBySearchText()
-            .then(imageUrl => {
-                console.log(`Image URL received in UI - ${imageUrl}`)
-                setImageUrl(imageUrl)
-            })
-            .catch(err => {
-                console.log("error encountered = " + err);
-                setImageUrl(DEFAULT_IMAGE_TO_SHOW);
-            });
-    }, [])
-
-    return (
-        <div className="App">
-        <Header />
-        <img
-        className='placeholder-gif' src={imageUrl} alt="placeholder-gif"/>
-        <p>Site under construction.</p>
-        </div>
-    );
+	return (
+        	<div className="App">
+        	<Header />
+        	<img className='placeholder-gif' src='https://media.giphy.com/media/fVeAI9dyD5ssIFyOyM/giphy.gif' alt="placeholder-gif"/>
+        	<p>Site under construction.</p>
+        	</div>
+    	);
 
 }
 
