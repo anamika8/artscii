52e147adc6c05c0920bfef497414c559ca82c012
Eliza Sohn <eliza.sohn@gmail.com>
2023-02-16 02:30:26 -0800

    Create Main.js

---
+++ artscii/src/components/main/Main.js
@@ -0,0 +1,12 @@
+import './Main.css';
+
+function Main() {
+    return (
+        <div className='main'>
+            <img className='placeholder-gif' src='https://media.giphy.com/media/fVeAI9dyD5ssIFyOyM/giphy.gif' alt="placeholder-gif"/>
+         	<p>Site under construction.</p>
+        </div>
+    );
+};
+
+export default Main;
