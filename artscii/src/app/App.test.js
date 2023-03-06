8ba29d71c6e0b3d56ddaa31f93341356c008b35b
Stuart Rimel <78074492+srimel@users.noreply.github.com>
2023-03-05 23:26:51 -0800

    Configure jest in package.json and create unit test for App rendering (#57)
    * Update package.json with jest configuration
    
    * Modified unit test for App component

--- artscii/src/app/App.test.js
+++ artscii/src/app/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders app component", () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const appElement = document.querySelector(".App");
+  expect(appElement).toBeInTheDocument();
 });
