a8d050eab1b238472771827d643292599f3299df
Eliza Sohn <56556859+elizasohn@users.noreply.github.com>
2023-03-19 17:53:46 -0700

    Feature/ascii test (#71)
    * Create dummyImgSrc.js
    
    * Create Asciify.test.js
    
    * install canvas

---
+++ artscii/src/tests/Asciify.test.js
@@ -0,0 +1,43 @@
+import { expect } from '@jest/globals';
+import convertToGrayScales from '../services/convertToGrayScales';
+import dummyImgSrc from './dummyImgSrc';
+import drawAscii from '../services/drawAscii';
+
+const image = new Image();
+image.src = dummyImgSrc;
+const width = 256;
+const height = 128;
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
+image.onload = () => {
+    canvas.width = width;
+    canvas.height = height;
+    context.drawImage(image, 0, 0, width, height);
+};
+const imageData = context.getImageData(0, 0, width, height);
+const grayScales = convertToGrayScales(context, imageData);
+
+describe('Convert to Gray Scales - grayScales Has Values', () => {
+    test('should return grayScales', () => {
+        expect(grayScales).not.toBeNull;
+    });
+});
+
+describe('Convert to Gray Scales Each RGB value synced', () => {
+    test('should return grayScales', () => {
+        let allRGBValuesCombineToGray = true;
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            if (!grayScales[i] === grayScales[i+1] && grayScales[i+1] === grayScales[i+2]) {
+                allRGBValuesCombineToGray = false;
+            }
+        };
+        expect(allRGBValuesCombineToGray).toEqual(true);
+    });
+});
+
+describe('Draw Ascii returns text value', () => {
+    test('should return grayScales', () => {
+        const asciiText = drawAscii(grayScales, width);
+        expect(asciiText).not.toBeUndefined;
+    });
+});
